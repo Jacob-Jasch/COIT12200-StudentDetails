45408e9ff8a7a1786274e77a5a5c8623eb81deb0
Jacob-Jasch <jacob_jasch@outlook.com>
2025-03-21 14:36:15 +1000

    Added starting page

--- src/main/java/coit12200/studentdetails/App.java
+++ src/main/java/coit12200/studentdetails/App.java
@@ -17,8 +17,10 @@ public class App extends Application {
 
     @Override
     public void start(Stage stage) throws IOException {
-        scene = new Scene(loadFXML("primary"), 640, 480);
+        scene = new Scene(loadFXML("main"), 700, 400);
         stage.setScene(scene);
+        stage.setResizable(false);
+        stage.setTitle("Student Grade System");
         stage.show();
     }
 
--- src/main/java/coit12200/studentdetails/MainController.java
+++ src/main/java/coit12200/studentdetails/MainController.java
@@ -6,15 +6,41 @@ package coit12200.studentdetails;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 /**
  * FXML Controller class
  *
- * @author 12273526
+ * @author xEnde
  */
 public class MainController implements Initializable {
 
+
+    @FXML
+    private TextArea txaOutput;
+    @FXML
+    private Button btnDisplayGrades;
+    @FXML
+    private Button btnFindID;
+    @FXML
+    private Button btnMarkRange;
+    @FXML
+    private Button btnStats;
+    @FXML
+    private TextField txtStudentId;
+    @FXML
+    private TextField txtMarkLow;
+    @FXML
+    private TextField txtMarkHigh;
+    @FXML
+    private Button btnClear;
+    @FXML
+    private Button btnExit;
     /**
      * Initializes the controller class.
      */
@@ -23,4 +49,28 @@ public class MainController implements Initializable {
         // TODO
     }    
     
+    @FXML
+    private void displayGradeAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void findIdAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void resultsInRangeAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void displayStatsAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void clearAction(ActionEvent event) {
+    }
+
+    @FXML
+    private void exitAction(ActionEvent event) {
+    }
+
 }
