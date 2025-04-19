cf5d239eb269d49f5147b466909ff5d05b8cc1d0
Jacob-Jasch <jacob_jasch@outlook.com>
2025-04-19 15:40:05 +1000

    Phase 3.2 Testing added

--- src/test/java/coit12200/studentdetails/GradeAnalyserTest.java
+++ src/test/java/coit12200/studentdetails/GradeAnalyserTest.java
@@ -2,6 +2,7 @@ package coit12200.studentdetails;
 
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.Test;
 
@@ -97,4 +98,136 @@ public class GradeAnalyserTest {
         Student result = ga.find(id);
         assertEquals(expResult, result); 
     } 
+
+    @Test
+    public void averageMarkValidTest() {
+        Student[] data = {
+            new Student("S10", "Elizabeth", "Jenkins", 20, 28, 44,  "HD"),
+            new Student("S35", "Bruce", "Donaldson", 5, 16, 25,  "SA"),
+            new Student("S20", "David", "Brown", 12, 9, 25,  "F")
+        };
+        GradeAnalyser ga = new GradeAnalyser(data);
+        double expResult = (20 + 28 + 44 + 5 + 16 + 25 + 12 + 9 + 25) / (double) data.length;
+        double result = ga.averageMark();
+        assertEquals(expResult, result); 
+    }
+
+    @Test
+    public void averageMarkEmptyTest() {
+        Student[] data = {};
+        GradeAnalyser ga = new GradeAnalyser(data);
+        assertThrows(EmptyListException.class, () -> ga.averageMark());
+    }
+
+    @Test
+    public void averageNegativeTest() {
+        GradeAnalyser ga = new GradeAnalyser(new Student[]{
+            new Student("S10","E","J",-20,-28,-44,"HD"),
+            new Student("S35","B","D", -5,-16,-25,"SA"),
+            new Student("S20","D","B", -12,-9,-25,"F")
+        });
+        assertEquals(-61.3333333333, ga.averageMark(), 1e-9);
+    }
+
+    @Test
+    public void medianMarkValidTest() {
+        Student[] data = {
+            new Student("S10", "Elizabeth", "Jenkins", 20, 28, 44,  "HD"),
+            new Student("S35", "Bruce",     "Donaldson", 5,  16, 25,  "SA"),
+            new Student("S20", "David",     "Brown",     12, 9,  25,  "F")
+        };
+        GradeAnalyser ga = new GradeAnalyser(data);
+
+        double expResult = 46.0;
+        double result    = ga.medianMark();
+        assertEquals(expResult, result, 0.0001);
+    }
+    
+    @Test
+    public void medianMarkEmptyTest() {
+        Student[] data = {};
+        GradeAnalyser ga = new GradeAnalyser(data);
+        assertThrows(EmptyListException.class, () -> ga.medianMark());
+    }
+
+    @Test
+    public void medianNegativeTest() {
+        GradeAnalyser ga = new GradeAnalyser(new Student[]{
+            new Student("S10","Elizabeth","Jenkins",-20,-28,-44,"HD"),
+            new Student("S35","Bruce","Donaldson", -5,-16,-25,"SA"),
+            new Student("S20","David","Brown",   -12,-9,-25,"F")
+        });
+        assertEquals(-46.0, ga.medianMark(), 0.0001);
+    }
+    
+
+    @Test
+    public void maximumValidTest() {
+        Student[] data = {
+            new Student("S10", "Elizabeth", "Jenkins", 20, 28, 44,  "HD"),
+            new Student("S35", "Bruce",     "Donaldson", 5,  16, 25,  "SA"),
+            new Student("S20", "David",     "Brown",     12, 9,  25,  "F")
+        };
+        GradeAnalyser ga = new GradeAnalyser(data);
+
+        int expResult = 92;
+        int result    = ga.Maximum();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void maximumEmptyTest() {
+        Student[] data = {};
+        GradeAnalyser ga = new GradeAnalyser(data);
+        assertThrows(EmptyListException.class, () -> ga.Maximum());
+    }
+
+    @Test
+    public void maximumNegativeTest() {
+        Student[] data = {
+            new Student("S10","Elizabeth","Jenkins",-20,-28,-44,"HD"),
+            new Student("S35","Bruce","Donaldson", -5,-16,-25,"SA"),
+            new Student("S20","David","Brown",   -12,-9,-25,"F")
+        };
+        GradeAnalyser ga = new GradeAnalyser(data);
+    
+        int expResult = -46;
+        int result    = ga.Maximum();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void minimumValidTest() {
+        Student[] data = {
+            new Student("S10", "Elizabeth", "Jenkins", 20, 28, 44,  "HD"),
+            new Student("S35", "Bruce",     "Donaldson", 5,  16, 25,  "SA"),
+            new Student("S20", "David",     "Brown",     12, 9,  25,  "F")
+        };
+        GradeAnalyser ga = new GradeAnalyser(data);
+
+        int expResult = 46;
+        int result    = ga.Minimum();
+        assertEquals(expResult, result);
+    }
+
+    @Test
+    public void minimumEmptyTest() {
+        Student[] data = {};
+        GradeAnalyser ga = new GradeAnalyser(data);
+        assertThrows(EmptyListException.class, () -> ga.Minimum());
+    }
+
+    @Test
+    public void minimumNegativeTest() {
+        Student[] data = {
+            new Student("S10","Elizabeth","Jenkins",-20,-28,-44,"HD"),
+            new Student("S35","Bruce","Donaldson", -5,-16,-25,"SA"),
+            new Student("S20","David","Brown",   -12,-9,-25,"F")
+        };
+        GradeAnalyser ga = new GradeAnalyser(data);
+    
+        int expResult = -92;
+        int result    = ga.Minimum();
+        assertEquals(expResult, result);
+    }
 }
